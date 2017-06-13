9935ce456288da7d66ddb17c9bb3f110c5673c39
Alex Gunning <alexandergunning@gmail.com>
2017-06-13 09:19:57 -0500

    Added pga rankings to DB

--- app/main.py
+++ app/main.py
@@ -31,13 +31,26 @@ def get_mlb_data():
     mlb = mongo.db.mlb
     players = mongo.db.players
     output = []
-    # for team in mlb.find().sort({"won":1}):
-    print(mlb.find)
     for team in mlb.find().sort("win_precentage", -1):
         name = players.find_one({"_id": team["_id"]})["name"]
         output.append({'name': name, 'team' : team['team'], 'won' : team['won'], 'lost' :\
         team['lost'], 'win_precentage' : team['win_precentage']})
     return render_template('layout.html', teams=output, table_name="MLB")
+
+@app.route('/pga', methods=['GET'])
+def get_pga_data():
+    """Get pga data"""
+    pga = mongo.db.pga
+    players = mongo.db.players
+    output = []
+    for golfer in pga.find().sort("ranking", 1):
+        #Get player name asscoiated with golfer
+        name = players.find_one({"_id": golfer["_id"]})["name"]
+        output.append({'name': name, 'golfer' : golfer['golfer'], 'ranking' : golfer['ranking']})
+
+    return render_template('pga_layout.html', golfers=output, table_name="PGA")
+
+# get_pga_data()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=8000)
---
+++ app/scraper/pga.py
@@ -0,0 +1,83 @@
+"""Pull down NBA data"""
+import re
+import requests
+from flask_pymongo import MongoClient
+from lxml import html
+
+client = MongoClient()
+db = client.fantasy
+
+print("runnning pga")
+
+#TODO Tie ids to a new object id and associate it with a user id
+
+def create_pga_db():
+    """Makes a new database for the nba"""
+    golfers = ["Dustin Johnson", "Rory McIlroy", "Jason Day", "Sergio Garcia",\
+     "Jordan Spieth", "Rickie Fowler", "Justin Rose", "Adam Scott",\
+     "Paul Casey", "Matt Kuchar", "Kevin Kisner", "Phil Mickelson"]
+
+    players = db.players
+    pga = db.pga
+    cursor = players.find()
+    for golfer, document in zip(golfers, cursor):
+        data = {"_id": document['_id'], "golfer": golfer}
+        _ = pga.insert(data)
+
+
+#Get the json data for the progam
+def get_player_ranking():
+    """Get the json data for the progam"""
+    pga = db.pga
+    res = requests.get("http://www.pgatour.com/stats/stat.186.html")
+    tree = html.fromstring(res.content)
+
+    web_ids = [str(golfers["web_id"]) for golfers in pga.find()]
+
+    #Get the first child of the talbe row element aand string the whitespace
+    for web_id in web_ids:
+        web_id_path = "playerStatsRow%s"%web_id
+        ranking = tree.get_element_by_id(web_id_path)[0].text_content().strip()
+        ranking = int(ranking)
+        pga.update_one({
+            'web_id': web_id
+        }, {
+            '$set': {
+                'ranking': ranking
+            }
+        })
+
+def scrape_player_ids():
+    """Get the pga tour webpage id's"""
+    res = requests.get("http://www.pgatour.com/stats/stat.186.html")
+    tree = html.fromstring(res.content)
+    pga = db.pga
+    golfers = [str(golfers["golfer"]) for golfers in pga.find()]
+    table = tree.find_class("details-table-wrap")
+
+    #TODO CLEAN UP wasted loop cycles
+    #Go through each link in table
+    for link in table:
+        counter = 0
+        for j in link.iterlinks():
+            #Get the text content and sanitize it
+            name = str((j[0].text_content())).strip().replace('\xa0', ' ')
+            if name in golfers:
+                href = str(j[2])
+                digits = re.findall(r"\d+", href)[0]
+                pga.update_one({
+                    'golfer': name
+                }, {
+                    '$set': {
+                        'web_id': digits
+                    }
+                })
+                #Break from loop if all golfers updated
+                counter += 1
+                if counter == len(golfers):
+                    break
+
+scrape_player_ids()
+get_player_ranking()
+
+print("done")
