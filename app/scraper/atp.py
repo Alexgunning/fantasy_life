060af8f2dc635dfa569f0a497d39109a337fd0c3
Alex Gunning <alexandergunning@gmail.com>
2017-06-14 01:29:42 -0500

    Pulled down rankings for atp

--- app/scraper/atp.py
+++ app/scraper/atp.py
@@ -13,39 +13,45 @@ print("runnning atp")
 
 def create_atp_db():
     """Makes a new database for the nba"""
-    player_names = ["Andy Murray", "Rafael Nadal", "Stan Wawrinka", "Novak Djok\
+    player_view_names = ["Andy Murray", "Rafael Nadal", "Stan Wawrinka", "Novak Djok\
     ovic", "Dominic Thiem", "Grigor Dimitrov", "David Goffin", "Tomas Berdych",\
     "Gael Monfils", "Jack Sock", "Nick Kyrgios", "Albert Ramos-Vinolas"]
 
+    player_web_names = [player.lower().replace(' ', '-') for player in player_view_names]
+
     players = db.players
     atp = db.atp
-    cursor = players.find()
-    for player, fantasy_player_name in zip(player_names, cursor):
-        data = {"associtaed_player_id": fantasy_player_name['_id'], "tennis_player": player}
+    users = players.find()
+    for player_web_name, player_view_name, users in zip(player_web_names, player_view_names, users):
+        data = {"associtaed_player_id": users['_id'], "player_web_name": player_web_name, "player_view_name" : player_view_name}
         _ = atp.insert(data)
 
 
 #Get the f data for the progam
+def get_ranking(element):
+    """Get the ranking from the correspoding link element"""
+
 
 def get_player_ranking():
-    """Get the json data for the progam"""
+    """get the json data for the progam"""
     res = requests.get("http://www.espn.com/tennis/rankings")
     tree = html.fromstring(res.content)
 
     atp = db.atp
+    players = db.players
 
-    tennis_players = [str(tennis_player["tennis_player"]) for tennis_player in atp.find()]
-
-    #Example
-    #andy-murray
-    player_web_names = [player.lower().replace(' ', '-') for player in tennis_players]
-    print(player_web_names)
+    tennis_players = [str(tennis_player["player_web_name"]) for tennis_player in atp.find()]
 
     tree = html.fromstring(res.content)
     table = tree.find_class("tablehead")
     player_links = [player_link for player_link in table[0].iterlinks() if player_link[1] == 'href']
+
     for link in player_links:
-        print(link)
+        player_link_name = link[2].split('/')[-1]
+        if player_link_name in tennis_players:
+            print(link[0].getparent().getprevious().text_content())
+            print(player_link_name)
 
 get_player_ranking()
+# create_atp_db()
 
