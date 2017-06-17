ff52b699229a44174ee6b214069f13308b8475f8
Alex Gunning <alexandergunning@gmail.com>
2017-06-17 02:03:30 -0500

    Completed stocks pulling down data implmenetation. Very slow could do some easy optimizations.

--- app/scraper/stocks.py
+++ app/scraper/stocks.py
@@ -1,24 +1,29 @@
+"""Pulling down data for stocks"""
 import datetime
 import requests
-import re
 from flask_pymongo import MongoClient
+import time
 
 client = MongoClient()
 db = client.fantasy
 
 # quarter three start
-# tracking delta between now and start 
+# tracking delta between now and start
 
-QUARTER_THREE_START = datetime.date(2016,10,1)
+QUARTER_THREE_START = datetime.date(2016, 10, 3)
 CURRENT_DATE = datetime.datetime.now()
 
 print(CURRENT_DATE)
 
-api_key = "hssemltznjvcx2shmdgt"
+api_key = "h81FgPPmka77HPc_Mnkj"
+
+
+#TODO Change to use Quandl library and may be able to bundle requests
 
 def create_stock_db():
     """Make new database for stocks"""
-    ticker_symbols = ["A", "AAPL", "C", "FB", "GOOG", "HOG", "HPQ", "INTC", "KO", "LUV", "MMM", "MSFT"]
+    ticker_symbols = ["A", "AAPL", "FB", "GOOG", "HOG", "HPQ", "INTC", "KO", "LUV",\
+     "MMM", "MSFT", "WMT"]
 
     players = db.players
     stock = db.stock
@@ -31,12 +36,13 @@ def create_stock_db():
 def get_stock_metadata():
     """Get stock metadata (Full company names)"""
     stock = db.stock
-    print('alex')
     ticker_symbols = [stocks['ticker_symbol'] for stocks in stock.find()]
+
     for ticker_symbol in ticker_symbols:
-        query =  "https://www.quandl.com/api/v3/datasets/WIKI/%s/metadata.json?api_key=HsseMLtznjVcx2sHmDGt"%ticker_symbol
+        query = "https://www.quandl.com/api/v3/datasets/WIKI/%s/metadata.json?api_key=%s"%(ticker_symbol,api_key)
         stock_name = requests.get(query).json()['dataset']['name']
-        shortened_stock_name = stock_name.split('(')[0]
+        shortened_stock_name = stock_name.split('(')[0].strip()
+        print(shortened_stock_name)
         stock.update_one({
             'ticker_symbol': ticker_symbol
             }, {
@@ -48,8 +54,8 @@ def get_stock_metadata():
 def stock_data(stock):
     """Get stock data based on stock ticker"""
 
-    query = "https://www.quandl.com/api/v3/datasets/WIKI/%s/data.json?api_key\
-    =HsseMLtznjVcx2sHmDGt&start_date=2014-01-01"%stock
+    query = "https://www.quandl.com/api/v3/datasets/WIKI/%s/data.json?api_key=%s"%(stock, api_key)
+    print(query)
 
     daily_results = requests.get(query).json()['dataset_data']['data']
     #Get the most recent price (first in array)
@@ -58,17 +64,20 @@ def stock_data(stock):
     #TODO Binary search
     #Or find postion based on start, stop and frequency
     for day in daily_results:
-        if day == str(QUARTER_THREE_START):
-            start_price = float(day[0][4])
+        if day[0] == str(QUARTER_THREE_START):
+            # start_price = float(day[0][4])
+            start_price = float(day[4])
             break
     delta = (current_price-start_price)/start_price
-    return {"ticker_symbol": stock, "current_price" : current_price, "start_price" : start_price, "delta" : delta}
+    return {"ticker_symbol": stock, "current_price" : current_price, "start_price" : start_price,\
+    "delta" : delta}
 
 def update_stock_data():
+    """Get all off the stocks data and update the database"""
     stock = db.stock
     ticker_symbols = [stocks['ticker_symbol'] for stocks in stock.find()]
-    stock_data = [stock_data(ticker_symbol) for ticker_symbol in ticker_symbols] 
-    for stock_price in stock_data:
+    stock_info = [stock_data(ticker_symbol) for ticker_symbol in ticker_symbols]
+    for stock_price in stock_info:
         stock.update_one({
             'ticker_symbol': stock_price['ticker_symbol']
             }, {
@@ -76,12 +85,13 @@ def update_stock_data():
                     'current_price': stock_price['current_price'],
                     'start_price': stock_price['start_price'],
                     'delta': stock_price['delta']
-            }
+                }
             })
 
 
-create_stock_db()
-get_stock_metadata()
+# create_stock_db()
+# get_stock_metadata()
+update_stock_data()
 
 #print(res['data'])
 # A – Agilent Technologies
